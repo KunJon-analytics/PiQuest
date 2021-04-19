571118b913230adae70481264bfb6fd6dfa992cb
KunJon Analytics <johnadelakun1@gmail.com>
2021-04-19 06:12:27 +0100

    aws media storage

--- PiQuest/settings.py
+++ PiQuest/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'guardian',
     'mathfilters',
     'pinax.badges',
+    'storages',
 ]
 
 SITE_ID=1
@@ -255,6 +256,13 @@ if not DEBUG:
     EMAIL_PORT = env("EMAIL_PORT")
     DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_FILE_OVERWRITE = False
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_REGION_NAME = 'FRA1'
+    AWS_S3_ENDPOINT_URL = 'https://${AWS_S3_REGION_NAME}.digitaloceanspaces.com'
 
 LOGGING = {
     'version': 1,
