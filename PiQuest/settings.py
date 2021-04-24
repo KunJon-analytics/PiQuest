476c1b5b70413c891ef07ea16aeb24e78f28de60
KunJon Analytics <johnadelakun1@gmail.com>
2021-04-24 11:47:21 +0100

    authenticate profile

--- PiQuest/settings.py
+++ PiQuest/settings.py
@@ -245,7 +245,7 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = "DENY"
 
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ['.piquests.com']
     SITE_ID=2
 
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
