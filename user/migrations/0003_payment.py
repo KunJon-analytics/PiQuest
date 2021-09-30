425dbae9546b7a5f77a784082b8e872847086ad9
John Adelakun <johnadelakun1.com>
2021-09-30 14:38:18 +0200

    trying to solve migration issues

--- user/migrations/0003_payment.py
+++ user/migrations/0003_payment.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='Payment',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('transaction_id', models.CharField(max_length=32)),
+                ('transaction_id', models.CharField(max_length=50)),
                 ('amount', models.FloatField()),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
